c1f7f1b698d3786a5a107dffc51454a49b117f30
NathanHuang86 <78175507+NathanHuang86@users.noreply.github.com>
2024-04-18 22:34:05 -0500

    Revert "Revert "Merge pull request #37 from hergotta24/Claire-Sprint-8""
    This reverts commit 923950f079aaecd7fbb0240318b208ef8a4de8e7.

--- EcommercePlatform/urls.py
+++ EcommercePlatform/urls.py
@@ -50,5 +50,7 @@ urlpatterns = [
                   path('payment-failed/<int:invoice_id>', payment_failed_view, name='payment-failed'),
                   # path('download/invoice/<int:invoice_id>', generate_invoice, name='download_invoice'),
                   path('add-shipping-details/', add_shipping_details, name='add_shipping_details'),
-                  path('remove-from-cart/<int:product_id>/', removeFromCart, name='removeFromCart')
+                  path('remove-from-cart/<int:product_id>/', removeFromCart, name='removeFromCart'),
+                  path('verification/', include('verify_email.urls')),
+                  path('invoice/<int:invoice_id>/generate_invoice_pdf/', generate_invoice_pdf, name='generate_invoice_pdf'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
